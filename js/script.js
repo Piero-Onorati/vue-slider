2cccf0615c9371bcf7ffb4abfe602a5bb75d3a21
Piero <piero.onorati@virgilio.it>
2021-06-22 18:53:15 +0200

    add new solution for bonus + add comments at script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const app = new Vue(
 
         data: {
             counter: 0,
-            timer:0,
+            timer: '',
             sliders: [
                 'img/img-1.jpg',
                 'img/img-2.jpg',
@@ -12,26 +12,34 @@ const app = new Vue(
                 'img/img-4.jpg'
             ],
             active: 'active'
-
         },
-        
+
         methods: {
+            // function for next slide
             next: function(){
                 (this.counter == this.sliders.length - 1) ? this.counter = 0 : this.counter++;
             },
+
+            // function for previous slide
             prev: function(){
                 (this.counter <= 0) ? this.counter = this.sliders.length - 1 : this.counter--;
             },
 
+            // function for dot of nav(each dot is a different image)
             dotClick: function(index){
                 this.counter = index;
             },
 
+            //function for autoplay
             play: function() {
-                let app = this;
-                this.timer = setInterval(function() {
-                  app.next();
-                }, 3000);
+
+                this.timer = setInterval(this.next, 3000)
+
+                //alternative solution
+                // let app = this;
+                // this.timer = setInterval(function() {
+                //   app.next();
+                // }, 3000);
             }
         },
 
