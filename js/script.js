221a2140f863705215ae7459c2c453bb6a5f652c
Piero <piero.onorati@virgilio.it>
2021-06-22 15:50:40 +0200

    add function for slider

--- js/script.js
+++ js/script.js
@@ -2,10 +2,23 @@ const app = new Vue(
     {
         el: '#app',
         data: {
+            counter: 0,
+            sliders: [
+                'img/img-1.jpg',
+                'img/img-2.jpg',
+                'img/img-3.jpg',
+                'img/img-4.jpg'
+            ]
 
         },
         methods: {
-            
+            next: function(){
+                (this.counter == this.sliders.length - 1) ? this.counter = 0 : this.counter++;
+            },
+            prev: function(){
+                (this.counter <= 0) ? this.counter = this.sliders.length - 1 : this.counter--;
+            }
+
         }
     }
 );
